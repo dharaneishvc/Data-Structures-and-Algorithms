3b3003ea2282d156c464459b25b50ef1515286d7
Dharaneish V C <dharaneishvc@gmail.com>
2022-11-12 17:01:14 +0530

    Update sat

--- Graph based Email suggestion/README.md
+++ Graph based Email suggestion/README.md
@@ -1,11 +1,25 @@
-#Auto Mail Match suggestion
+#Auto Mail Match suggestion and “Who knows Who”
 
-While the user type to-address or search for mail users, this product predicts and displays the perfect match for input based on the previous sent data of all mail users. It displays in the order the mail is very close to a user or not.
+While the user type to-address or search for mail users, this feature predicts and displays the perfect match for input based on the previous sent data of all mail users. It displays in the order the mail is very close to a user or not.
+When User types destination address fully, this feature displays the connection between both users if exist and the path they are connection similar to mutual friends and followers in social media platforms.
 Dataset description:
 https://snap.stanford.edu/data/email-EuAll.html
 This dataset contains anonymized information about all incoming and outgoing email of a large European research institution for a period from October 2003 to May 2005 (18 months). For each sent or received email message the dataset contains the time, the sender and the recipient of the email. Overall, it contains 3,038,531 emails between 287,755 different email addresses. Note that the dataset contains only complete email graph for only 1,258 email addresses that come from the research institution. Furthermore, there are 34,203 email addresses that both sent and received email within the span of our dataset. All other email addresses are either non-existing, mistyped or spam.
 Given a set of email messages, each node corresponds to an email address and a directed edge between nodes i and j, if i sent at least one message to j.
 
-#Choosing Graph Representation
+##Choosing Graph Representation
 Adjacency list is the perfect representation of graph for this method. An adjacency list is efficient in terms of storage because we only need to store the values for the edges. Our dataset is a sparse graph with millions of vertices and edges, this can mean a lot of saved space.  Meanwhile VxV space requirement of the adjacency matrix makes it a memory hog. Our dataset Email Graphs don't have too many connections, and this causes wastage of space. Hence adjacency lists are the better choice for most tasks.
-It also helps to find all the vertices adjacent to a vertex easily.
+It also helps to find all the vertices adjacent to a vertex easily. For Breadth First and Depth First search techniques, Adjacency list is perfect way as it can be traversed easily.
+
+##Algorithm
+1)	Input: Read from files and store in the form of Adjacency list as it is the best representation.
+2)	Print Graph: Traverse each vertex and print the vertexes which has direct edge link with it.
+3)	Mail Suggestion: Do Breadth-First Traversal between with the input as source and print the node elements in order
+4)	Check Connection: Calculate the minimum distance between two nodes by doing Breadth-First Traversal from the source vertex and by adding up in distances in each step. At the time of BFS maintain an array of distance[n] and initialize it to zero for all vertices and initialize Found to No for connection checking. Now, suppose during BFS, vertex x is popped from queue and we are pushing all adjacent non-visited vertices(y) back into queue at the same time we should update distance[y] = distance[x] + 1; Also, during the BFS if the second vertex is found in traversal, set found to Yes.
+5)	Who Knows Who: Print all path from given source to destination. Do Depth First Traversal of a given directed graph starting from the source. Keep storing the visited vertices in an array. If the destination vertex is reached, print the contents of the array. Also mark current vertices in the array[] as visited also so that the traversal doesn’t go in a cycle.
+6)	It works for Both Undirected and Directed Graphs. For Directed Graphs we can also add both from and to as edges so that we get better result.
+
+##Complexity Analysis
+•	If E is the number of edges and V is number of nodes in a graph, then the time complexity of building such a list is O(E). The space complexity is O(V + E). But, in the worst case of a complete graph, which contains V/2 edges, the time and space complexities reduce to O(n^2)
+•	Time Complexity for Mail suggestion and Connection checking is same as that of Breadth-First Traversal ie O(V+E). And auxiliary space required is O(V) as V vertices can be present in the queue or array in the worst case.
+•	Time Complexity for Who knows Who is O(V^V), The time complexity is exponential. From each vertex there are V vertices that can be visited from current vertex. Auxiliary space required is O(V^V), To store the paths V^V space is needed.
--- Graph based Email suggestion/graphemail.py
+++ Graph based Email suggestion/graphemail.py
@@ -21,7 +21,7 @@ class Graph:
     node.next = self.graph[d]
     self.graph[d] = node
 
-    # Print the graph
+  # Print the graph
   def print_agraph(self):
     for i in range(self.V):
       print("Vertex " + str(i) + ":", end="")
@@ -31,9 +31,101 @@ class Graph:
         temp = temp.next
       print(" \n")
 
+  def BFS(self, s):
+    # Mark all the vertices as not visited
+    visited = [False] * (self.V)
+
+    # Create a queue for BFS
+    queue = []
+
+    # Mark the source node as visited and enqueue it
+    queue.append(s)
+    visited[s] = True
+
+    while queue:
+
+      # Dequeue a vertex from queue and print it
+      s = queue.pop(0)
+      print (s, end = ",")
+
+      # Get all adjacent vertices of the dequeued vertex s. If a adjacent has not been visited, then mark it visited and enqueue it
+      nd = self.graph[s]
+      while nd:
+        if visited[nd.vertex] == False:
+          queue.append(nd.vertex)
+          visited[nd.vertex] = True
+        nd = nd.next
+
+  def minEdgeBFS(self, s, d):
+    # Mark all the vertices as not visited
+    visited = [False] * (self.V)
+
+    #Pathexist Boolean Value
+    pathexist = False
+
+    # Initialize distances as 0
+    distance = [0] * (self.V)
+
+    # Create a queue for BFS
+    queue = []
+
+    # Mark the source node as visited and enqueue it
+    queue.append(s)
+    visited[s] = True
+
+    while queue:
+
+      # Dequeue a vertex from queue and print it
+      s = queue.pop(0)
+      if s == d:
+        pathexist = True
+
+      # Get all adjacent vertices of the dequeued vertex s. If a adjacent has not been visited, then mark it visited and enqueue it
+      nd = self.graph[s]
+      while nd:
+        if visited[nd.vertex] == False:
+          queue.append(nd.vertex)
+          visited[nd.vertex] = True
+          distance[nd.vertex] = distance[s] + 1
+        nd = nd.next
+        
+    return [pathexist, distance[d]]
+
+  def printAllPathsUtil(self, u, d, visited, path):
+
+      # Mark the current node as visited and store in path
+      visited[u]= True
+      path.append(u)
+
+      # If current vertex is same as destination, then print current path[]
+      if u == d:
+          print (path)
+      else:
+          # If current vertex is not destination Recur for all the vertices adjacent to this vertex
+          nd = self.graph[u]
+          while nd:
+            if visited[nd.vertex]== False:
+              self.printAllPathsUtil(nd.vertex, d, visited, path)
+            nd = nd.next
+                    
+      # Remove current vertex from path[] and mark it as unvisited
+      path.pop()
+      visited[u]= False
+
+  def printAllPaths(self, s, d):
+ 
+    # Mark all the vertices as not visited
+    visited =[False]*(self.V)
+
+    # Create an array to store paths
+    path = []
+
+    # Call the recursive helper function to print all paths
+    self.printAllPathsUtil(s, d, visited, path)
+
 if __name__ == "__main__":
   #TESTING WITHOUT FILE
-  '''
+  
   V = 5
   # Create graph and edges
   graph = Graph(V)
@@ -41,6 +133,11 @@ if __name__ == "__main__":
   graph.add_edge(0, 2)
   graph.add_edge(0, 3)
   graph.add_edge(1, 2)
+  graph.add_edge(2, 0)
+  graph.add_edge(2, 3)
+  graph.add_edge(3, 3)
+  graph.add_edge(4, 3)
+
   '''
 
   filelink = open("Email-EuAll.txt","r")
@@ -59,5 +156,44 @@ if __name__ == "__main__":
     # use realine() to read next line
     file_line = filelink.readline()
   filelink.close()
-  
-  graph.print_agraph()
+
+'''
+
+  op = 0
+  while op != 5:
+    print('\n', '─' * 50)
+    op = int(input("1. Print Graph \n2. Find Suggestions \n3. Distance between two Nodes(if exists) \n4. All Path between Nodes \n5. Exit \nEnter(1-5): "))
+    if(op == 1):
+      print("Graph Adjacency List:")
+      graph.print_agraph()
+    elif(op == 2):
+      inn = int(input("Enter your Mail (Node Number):"))
+      while (inn >= V or inn < 0):
+        inn = int(input("Error! Again Enter your Mail (Node Number (0-V)):"))
+      print("The order of Suggestion is: ", end=" ")
+      graph.BFS(inn)
+    elif(op == 3):
+      ins = int(input("Enter Source Mail (Node Number):"))
+      while (ins >= V or ins < 0):
+        ins = int(input("Error! Again Enter Source (Node Number (0-V)):"))
+      ind = int(input("Enter Destination Mail (Node Number):"))
+      while (ind >= V or ind < 0):
+        ind = int(input("Error! Again Enter Destination (Node Number (0-V)):"))
+      a = graph.minEdgeBFS(ins,ind)
+      if a[0]:
+        print("Path Exists Between 2 points")
+        print("The distance between 2 nodes is: ", a[1])
+      else:
+        print("Path doesn't exist Between 2 points")
+    elif(op == 4):
+      ins = int(input("Enter Source Mail (Node Number):"))
+      while (ins >= V or ins < 0):
+        ins = int(input("Error! Again Enter Source (Node Number (0-V)):"))
+      ind = int(input("Enter Destination Mail (Node Number):"))
+      while (ind >= V or ind < 0):
+        ind = int(input("Error! Again Enter Destination (Node Number (0-V)):"))
+      print("Set of all paths between 2 nodes are: ")
+      graph.printAllPaths(ins, ind)
+    else:
+      print("Exit")
+      break
